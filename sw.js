77ba9ea90e9e37b44439656abcf929bbbf183dbb
N'Meboon <mmeboontest@gmail.com>
2026-05-29 07:17:59 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'nmes-v1';
-const ASSETS = ['/', '/index.html', '/app.js', '/style.css'];
-self.addEventListener('install',  e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch',    e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
